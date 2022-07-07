cd5f9d439fab5c9c4ef0fb3109cfa623e2e77265
sklee <leevinodi@gmail.com>
2022-07-07 15:42:03 +0900

    frontend init

---
+++ Frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Main from "./pages/Main";
+// import Login from "./pages/Login";
+// import LoginGoogle from "./pages/Login/Googlebutton";
+// import Admin from "./pages/Admin";
+// import Register from "./pages/Register";
+// import AdminRegister from "./pages/Register/Admin";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                {/* <Route exact path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/adminregister" element={<AdminRegister />} />
+                <Route path="/google-login" element={<LoginGoogle />} /> */}
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ Frontend/src/pages/Main/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Main() {
+  return <p style={{ fontSize: '0.8rem' }}>카카오 같이가치 클론 페이지</p>;
+}
