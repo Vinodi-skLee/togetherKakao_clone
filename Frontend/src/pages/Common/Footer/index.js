61a1a99d0fb49890973e62b2fcb44f92f9ce0839
sklee <leevinodi@gmail.com>
2022-07-13 18:09:49 +0900

    content, footer created

---
+++ Frontend/src/pages/Common/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../asset/css/app.css';
+// import { GoogleLogout } from 'react-google-login';
+
+export default function Footer() {
+  return (
+    <div className="footer_container">
+      <div id="akFoot">
+        <div className="inner_footer">
+          <a className="link_home" href="/" />
+          <div className="info_footer">
+            <p className="util_foot_desc">
+              당신의 착한 마음을 응원합니다.<br></br>더 좋은 세상을 꿈꾼다면 지금 시작해보세요.
+            </p>
+            <a className="util_foot" hred="/">
+              제안하기
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- Frontend/src/pages/Main/index.js
+++ Frontend/src/pages/Main/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Common/Header/index';
 import Content from '../Common/Content/index';
+import Footer from '../Common/Footer/index';
 
 export default function Main() {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   );
 }
