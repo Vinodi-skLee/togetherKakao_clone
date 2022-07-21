79a7d973c59a9e550fedde0a042359c5952066eb
sklee <leevinodi@gmail.com>
2022-07-21 16:07:43 +0900

    connected(back,front)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import Main from './pages/Main';
@@ -9,19 +10,41 @@ import Login from './pages/Login';
 // import Register from "./pages/Register";
 // import AdminRegister from "./pages/Register/Admin";
 
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Main />} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 function App() {
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    fetch('/hello')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMessage(data);
+      });
+  }, []);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        {/* <Route exact path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/adminregister" element={<AdminRegister />} />
-                <Route path="/google-login" element={<LoginGoogle />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        // 기본코드
+        <ul>
+          {message.map((v, idx) => (
+            <li key={`${idx}-${v}`}>{v}</li>
+          ))}
+        </ul>
+      </header>
+    </div>
   );
 }
 
